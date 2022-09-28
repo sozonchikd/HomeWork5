db6998798af5fc75dac293998ec1a5beb499d900
Sozonchik <den.sozon@mail.ru>
2022-09-28 15:50:17 +0500

    Сделала 5.1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // Домашка 5, условный оператор 2
         System.out.println("Задание 5.1");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
 
     }
 }
